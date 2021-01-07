4907e2206958be20694d2fcc8fe1b010fe67d440
drhaithammg <dr.haithammg@gmail.com>
2021-01-07 18:40:04 +0200

    Update main.py

--- packages/main/main.py
+++ packages/main/main.py
@@ -1,4 +1,13 @@
 import os
+from pathlib import Path
 # import packages.packageA.fileA
 # from ..packageB.fileB import g
+path = os.getcwd()
+os.chdir(path+'/packages/packageA')
 print(os.getcwd())
+# from ..packageB import g
+# path = path.split('/')
+# path_len = len(path)
+# print(path_len)
+# path = '/'.join(path[:5])
+# print(path)
